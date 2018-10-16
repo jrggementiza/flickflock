fdd8934ff49c5984de9dc787d6dc6295de944ace
jrggementiza <jrggementiza@gmail.com>
2018-10-16 14:29:38 +0800

    added functional group join view. need to add group validation / group email invite

--- flickflock/accounts/views.py
+++ flickflock/accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from django.contrib.sites.models import Site
 from .forms import UserRegistrationForm, GroupCreationForm
 from .models import Person
 
@@ -42,12 +43,10 @@ def groups(request):
 @login_required
 def groups_create(request):
     current_user = request.user
-    # current_user = Person.objects.get(username=request.user.username)
     # if already in a group
     if request.method == 'POST':
         form = GroupCreationForm(request.POST, request.FILES)
         # if group already created invalid
-
         if form.is_valid():
             group = form.save(commit=False)
             group.name = form.cleaned_data.get('name')
@@ -68,7 +67,20 @@ def groups_create(request):
 @login_required
 def groups_join(request):
     current_user = request.user
+    if request.method == 'POST':
+        # add group validation / password so not everyone can join
+        form = GroupCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save(commit=False)
+            name = form.cleaned_data.get('name')
+            group = Site.objects.get(name=name)
+            current_user.group = group
+            current_user.save()
+            return redirect('/')
+    else:
+        form = GroupCreationForm()
     context = {
+        'form': form,
         'current_user': current_user,
     }
     return render(request, 'accounts/groups_join.html', context)
